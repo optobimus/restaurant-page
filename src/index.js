60e8759322161be09f6e81ae7e8ec77508178149
Tobias <113835194+optobimus@users.noreply.github.com>
2022-12-27 20:48:30 +0100

    Create multiple export files for different pages Style the homepage completely, change background image and add import export functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import index from './home';
 
 const content = document.querySelector(".content");
 
@@ -19,30 +20,9 @@ nav.appendChild(nav2);
 nav.appendChild(nav3);
 header.appendChild(nav);
 
+content.appendChild(header);
 
-const main = document.createElement("div");
-main.classList.add("main");
-const welcome = document.createElement("p");
-welcome.classList.add("welcome");
-const restaurantName = document.createElement("h1");
-restaurantName.classList.add("name");
-const border = document.createElement("br");
-border.classList.add("border");
-const paragraph = document.createElement("p");
-paragraph.classList.add("paragraph");
-const btn = document.createElement("button");
-btn.classList.add("button");
-
-welcome.textContent = "Welcome";
-restaurantName.textContent = "Muye";
-paragraph.textContent = "The Muye in Brixen is a wonderful Chinese restaurant that serves authentic and delicious dishes. The friendly staff and cozy atmosphere make it a perfect spot for a relaxed dinner with friends or a romantic date.";
-btn.textContent = "View our menu";
-
-main.appendChild(welcome);
-main.appendChild(restaurantName);
-main.appendChild(border);
-main.appendChild(paragraph);
-main.appendChild(btn);
+index();
 
 const footer = document.createElement("div");
 footer.classList.add("footer");
@@ -52,6 +32,37 @@ footerText.textContent = "Made with ❤️ by optobimus";
 
 footer.appendChild(footerText);
 
-content.appendChild(header);
-content.appendChild(main);
 content.appendChild(footer);
+nav1.addEventListener("click", (e) => {
+    content.innerHTML = "";
+    const header = document.createElement("div");
+    header.classList.add("header");
+    const nav = document.createElement("ul");
+    nav.classList.add("nav");
+    const nav1 = document.createElement("li");
+    const nav2 = document.createElement("li");
+    const nav3 = document.createElement("li");
+
+    nav1.textContent = "HOME";
+    nav2.textContent = "MENU";
+    nav3.textContent = "ABOUT";
+
+    nav.appendChild(nav1);
+    nav.appendChild(nav2);
+    nav.appendChild(nav3);
+    header.appendChild(nav);
+
+    content.appendChild(header);
+
+    index();
+
+    const footer = document.createElement("div");
+    footer.classList.add("footer");
+    const footerText = document.createElement("p");
+    footerText.classList.add("footerText");
+    footerText.textContent = "Made with ❤️ by optobimus";
+
+    footer.appendChild(footerText);
+
+    content.appendChild(footer);
+});
