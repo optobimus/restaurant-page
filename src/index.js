f54bd89fcec9020e24a6990618869df3c31427d9
Tobias <113835194+optobimus@users.noreply.github.com>
2022-12-29 20:52:48 +0100

    Implement tabbed browsing by using modules

--- src/index.js
+++ src/index.js
@@ -1,47 +1,22 @@
 import './styles.css';
 import index from './home';
+import menu from './menu'
 
 const content = document.querySelector(".content");
-
-const header = document.createElement("div");
-header.classList.add("header");
-const nav = document.createElement("ul");
-nav.classList.add("nav");
-const nav1 = document.createElement("li");
-const nav2 = document.createElement("li");
-const nav3 = document.createElement("li");
-
-nav1.textContent = "HOME";
-nav2.textContent = "MENU";
-nav3.textContent = "ABOUT";
-
-nav.appendChild(nav1);
-nav.appendChild(nav2);
-nav.appendChild(nav3);
-header.appendChild(nav);
-
-content.appendChild(header);
-
-index();
-
-const footer = document.createElement("div");
-footer.classList.add("footer");
-const footerText = document.createElement("p");
-footerText.classList.add("footerText");
-footerText.textContent = "Made with ❤️ by optobimus";
-
-footer.appendChild(footerText);
-
-content.appendChild(footer);
-nav1.addEventListener("click", (e) => {
-    content.innerHTML = "";
+let menuBtn, homeBtn, aboutBtn;
+function createHeader () {
     const header = document.createElement("div");
     header.classList.add("header");
     const nav = document.createElement("ul");
     nav.classList.add("nav");
     const nav1 = document.createElement("li");
+    nav1.addEventListener("click", createHome);
+    nav1.classList.add("homeBtn");
     const nav2 = document.createElement("li");
+    nav2.addEventListener("click", createMenu);
+    nav2.classList.add("menuBtn");
     const nav3 = document.createElement("li");
+    nav3.classList.add("aboutBtn");
 
     nav1.textContent = "HOME";
     nav2.textContent = "MENU";
@@ -53,9 +28,10 @@ nav1.addEventListener("click", (e) => {
     header.appendChild(nav);
 
     content.appendChild(header);
+    return;
+}
 
-    index();
-
+function createFooter () {
     const footer = document.createElement("div");
     footer.classList.add("footer");
     const footerText = document.createElement("p");
@@ -63,6 +39,43 @@ nav1.addEventListener("click", (e) => {
     footerText.textContent = "Made with ❤️ by optobimus";
 
     footer.appendChild(footerText);
-
     content.appendChild(footer);
+    return;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    createHeader();
+
+    content.appendChild(index());
+    
+    createFooter();
+    homeBtn = document.querySelector(".homeBtn");
+    menuBtn = document.querySelector(".menuBtn");
+    aboutBtn = document.querySelector(".aboutBtn");
+
+
 });
+
+function createHome() {
+    content.innerHTML = "";
+    createHeader();
+    homeBtn = document.querySelector(".homeBtn");
+    menuBtn = document.querySelector(".menuBtn");
+    aboutBtn = document.querySelector(".aboutBtn");
+    content.appendChild(index());
+    
+    createFooter();
+}
+
+function createMenu(){
+    content.innerHTML = "";
+    createHeader();
+    homeBtn = document.querySelector(".homeBtn");
+    menuBtn = document.querySelector(".menuBtn");
+    aboutBtn = document.querySelector(".aboutBtn");
+    content.appendChild(menu());
+    console.log("Click");
+
+    createFooter();
+}
